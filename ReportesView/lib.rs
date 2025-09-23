84d1336f2b331b6347c6614960eb21f85a6c24fc
ioaquin <215891922+ioaquin@users.noreply.github.com>
2025-09-23 00:38:39 -0300

    Primer configuración compilable

---
+++ ReportesView/lib.rs
@@ -0,0 +1,34 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod ReportesView {
+    use ink::codegen::TraitCallBuilder;
+    use MarketPlace::SistemaRef;
+
+    #[ink(storage)]
+    pub struct ReportesView {
+        marketplace: SistemaRef
+    }
+
+    impl ReportesView {
+
+        // Instancia el marketplace y lo devuelve contenido en una estructura
+        // para referenciarlo
+        #[ink(constructor)]
+        pub fn new(marketplace_code_hash: Hash) -> Self {
+            let marketplace = SistemaRef::new()
+                .code_hash(marketplace_code_hash)
+                .endowment(0)
+                .salt_bytes([0xDE, 0xAD, 0xBE, 0xEF]) // Salt arbitrario
+                .instantiate();
+
+            Self { marketplace }
+        }
+
+        // Mensaje de prueba
+        #[ink(message)]
+        pub fn reportar(&mut self) -> u128 {
+            self.marketplace.get_proximo_id_orden()
+        }
+    }
+}
