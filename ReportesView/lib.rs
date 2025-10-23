1fa4bc5d06cbe2f3308fa85b7b4633169a096bcd
ioaquin <215891922+ioaquin@users.noreply.github.com>
2025-10-23 09:23:29 -0300

    Documentación

--- ReportesView/lib.rs
+++ ReportesView/lib.rs
@@ -32,26 +32,38 @@ mod ReportesView {
             Self { marketplace }
         }
 
-        #[ink(message)]
-        pub fn get_top5_compradores(&self) -> Result<Vec<Usuario>,ErrorSistema> {
-            self.marketplace.consultar_top_5_compradores()
-        }
-
+        /// Devuelve hasta 5 usuarios (Vendedor/Ambos) ordenados por puntaje como vendedor.
+        /// Solo accesible si se ha establecido ReportesView.
         #[ink(message)]
         pub fn get_top5_vendedores(&self) -> Result<Vec<Usuario>,ErrorSistema> {
             self.marketplace.consultar_top_5_vendedores()
         }
 
+        /// Devuelve hasta 5 usuarios (Vendedor/Ambos) ordenados por puntaje como vendedor.
+        /// Solo accesible si se ha establecido ReportesView.
         #[ink(message)]
-        pub fn get_estadisticas_por_categoria(&self) -> Result<Vec<(Categoria, u32, u8)>, ErrorSistema> { 
-            self.marketplace.estadisticas_por_categoria()
+        pub fn get_top5_compradores(&self) -> Result<Vec<Usuario>,ErrorSistema> {
+            self.marketplace.consultar_top_5_compradores()
         }
 
+        /// Devuelve una lista de hasta 10 productos de una categoría específica ordenados por ventas en forma descendente.
+        /// Solo accesible si se ha establecido ReportesView.
+        ///
+        /// # Parámetros
+        /// - `categoria`: Categoría de los productos a buscar.
+        ///
         #[ink(message)]
         pub fn get_productos_mas_vendidos(&self, categoria: Categoria) -> Result<Vec<(u128, u8)>, ErrorSistema> {
             self.marketplace.ver_productos_mas_vendidos(categoria)
         }
 
+        /// Devuelve un vector con las estadisticas por categoría, con la cantidad total de ventas y la suma de puntuaciones.
+        #[ink(message)]
+        pub fn get_estadisticas_por_categoria(&self) -> Result<Vec<(Categoria, u32, u8)>, ErrorSistema> { 
+            self.marketplace.estadisticas_por_categoria()
+        }
+
+        /// Devuelve un vector con la cantidad de órdenes de todos los usuarios registrados.
         #[ink(message)]
         pub fn get_ordenes_por_usuario(&self) -> Result<Vec<(AccountId, u128)>, ErrorSistema> {
             self.marketplace.cantidad_ordenes_por_usuario()
